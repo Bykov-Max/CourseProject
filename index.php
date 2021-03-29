d9c4f53d4272ff4edf0134cce4ec066e3c2c02a5
Max <Maxim1202@gmail.com>
2021-03-29 22:11:54 +0500

    saveAll

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="css1.css">
-</head>
-<body>
-    <header>
+<header>
         <div class="container">
             <a class="logo">LOGO</a>
             <div class="burger">
@@ -26,5 +15,3 @@
             <a href="#menu4">Тексты песен</a>
         </nav>
     </header>
-</body>
-</html>
