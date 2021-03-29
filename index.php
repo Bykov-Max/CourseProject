1e3c1b28e8481d19d4d092c351c11fa99001b953
Max <Maxim1202@gmail.com>
2021-03-29 22:14:43 +0500

    saveAll

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include $_SERVER["DOCUMENT_ROOT"]."/template/header.php"; ?>
 <header>
         <div class="container">
             <a class="logo">LOGO</a>
@@ -9,9 +10,10 @@
         </div>
 
         <nav class="block-menu">
-            <a href="#menu1"> <img src="logo(2).jpg"> </a>
+            <a href="#menu1"> <img src="logo(1).jpg"> </a>
             <a href="#menu2">Информация о группе</a>
             <a href="#menu3">Солисты</a>
             <a href="#menu4">Тексты песен</a>
         </nav>
-    </header>
+</header>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/template/footer.php"; ?>
